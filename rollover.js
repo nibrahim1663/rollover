6e7f34dddafce0ee5828df59ae88a39325d72448
Nawriz <nibrahim1663@conestogac.on.ca>
2022-06-08 10:38:57 -0400

    Final work

--- rollover.js
+++ rollover.js
@@ -2,19 +2,43 @@
 
 const $ = selector => document.querySelector(selector);
 
+// Search for ALL the image elements in the unordered list that has the id = "image_rollovers"
+const imgLink = $("#image_rollovers").querySelectorAll("img");
+
+// Set up a 1 second timer for the rollover images display
+let rolledOverImageTimer = 1000;
+
+// Set up a 2 seconds timer for the original images display
+let originalImageTimer = 2000;
+
+// Decalare a Global variable 
+let link = null;
+
+// This function is called to iterirate over the images source and upadte it
+const displayRolledOverImages = () =>
+{
+    for (link of imgLink) 
+    {
+        link.src = link.id;
+    }
+};
+
+// This function is called to update the images source to the original ones
+const displayOriginalImages = () =>
+{
+    document.getElementById("images/deer.jpg").src = "images/fishers.jpg";
+    document.getElementById("images/bison.jpg").src = "images/fisherFish.jpg";
+};
 
 document.addEventListener("DOMContentLoaded", () => 
 {
-    //const images = document.querySelectorAll("#image_rollovers img");
-    const imgLink = $("#image_rollovers").querySelectorAll("img");
-
     // process each img tag
-    for (let link of imgLink) 
+    for (link of imgLink) 
     {
         // Grab the original image's SRC and assign it to originalImage variable
         const originalImage = link.src; 
 
-        // Grab the rolled over image's SRC and assign it to rolledOverImage variable
+        // Grab the rolled over image's ID and assign it to rolledOverImage variable
         const rolledOverImage = link.id;
 
         // preload rollover image
@@ -22,11 +46,20 @@ document.addEventListener("DOMContentLoaded", () =>
         image.src = rolledOverImage;
 
         // set up event handlers for hovering an image
-        link.addEventListener("mouseover", () => {
+        link.addEventListener("mouseover", () => 
+        {
             link.src = rolledOverImage;
         });
-        link.addEventListener("mouseout", () => {
+        link.addEventListener("mouseout", () => 
+        {
             link.src = originalImage;
         });
     }
+
+    // Set up a timer that causes the rollover images to be displayed one second after the page is loaded.
+    setTimeout(displayRolledOverImages, rolledOverImageTimer);
+    
+    // Set up another timer that causes the original images to be displayed again two seconds after the page is loaded.
+    setTimeout(displayOriginalImages, originalImageTimer);
+
 });
